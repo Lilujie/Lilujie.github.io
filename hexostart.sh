dab8e939a21686c62077f1435a3d869336810217
lilj0302 <lilj0302@thundersoft.com>
2018-10-20 09:07:12 +0800

    config update

--- hexostart.sh
+++ hexostart.sh
@@ -1,15 +1,3 @@
-sudo apt-get install -y python-software-properties software-properties-common
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install nodejs
-sudo apt install nodejs-legacy
-sudo apt install npm
-sudo npm config set registry https://registry.npm.taobao.org
-sudo npm config list
-sudo npm install n -g
-sudo n stable
-sudo node -v
-sudo npm install hexo -g
 npm install hexo-deployer-git --save
 npm install hexo-asset-image --save
 npm install hexo-generator-sitemap --save
